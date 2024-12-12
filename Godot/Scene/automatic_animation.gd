4b243b48e549703acc61eaf2e7b7a9d367843166
Brawltest <158631597+Frakif91@users.noreply.github.com>
2024-12-12 19:11:16 +0100

    Update site visibility (1/2)

--- Godot/Scene/automatic_animation.gd
+++
@@ -1,7 +0,0 @@
-@tool
-extends AnimatedSprite3D
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	animation_changed.connect(func(): play(&""))
--- Godot/Scripts/entry_ui.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-var trans_ready_time = Globals.trans_ready_time
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	await get_tree().create_timer(1).timeout
-	($AnimationPlayer as AnimationPlayer).play(&"new_animation",-1,-1.0,true)
